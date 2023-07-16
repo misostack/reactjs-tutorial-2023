82ae36e3108d106dffa05088d95745f5f15de9c9
misostack <misostack.com@gmail.com>
2023-07-16 21:57:07 +0700

    add key

--- src/Lession003.jsx
+++ src/Lession003.jsx
@@ -1,39 +1,27 @@
 import { useState } from "react";
 import enviroment from "./shared/environment";
 
-const Lession003 = () => {
-  const [stateObject, setStateObject] = useState({
-    a: {
-      b: {
-        c: 1,
-      },
-    },
-  });
-  console.log("Before render, stateObject.a.b.c:", stateObject.a.b.c);
+const Counter = ({ number, children, ...props }) => {
   return (
     <>
-      <h2>State and Event</h2>
-      <h3>StateObject: {JSON.stringify(stateObject)}</h3>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={(e) => {
-          e.preventDefault();
-          setStateObject((obj) => {
-            console.log(obj.a.b.c, "obj === stateObject", obj === stateObject);
-            return {
-              ...obj,
-              a: {
-                b: {
-                  c: obj.a.b.c + 1,
-                },
-              },
-            };
-          });
-        }}
-      >
-        Increase counter
-      </button>
+      <a {...props}>
+        Counter Name:{children}
+        <br />
+        Value: {number}
+      </a>
+    </>
+  );
+};
+
+const Lession003 = () => {
+  const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return (
+    <>
+      {values.map((v, index) => (
+        <Counter key={index} number={v} className="btn btn-primary">
+          Counter {index + 1}
+        </Counter>
+      ))}
     </>
   );
 };
